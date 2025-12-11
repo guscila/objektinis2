55d37607563e9a6c912b60c0368820c7df663142
guscila <guscila@OKI>
2025-12-11 16:57:57 +0200

    atnaujintas run.bat failas

--- v0.1-naujas/run.bat
+++ v0.1-naujas/run.bat
@@ -3,5 +3,5 @@ if not exist build mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
-if exist .\v1.0.exe (.\v1.0.exe %*) else (.\Release\v1.0.exe %*)
+if exist .\v2.0.exe (.\v2.0.exe %*) else (.\Release\v2.0.exe %*)
 pause
